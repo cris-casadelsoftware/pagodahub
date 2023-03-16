19f3297254726506fc4b5e931d1296f712d1bc55
cris-casadelsoftware <cristopher@casadelsoftware.com>
2023-03-16 17:24:49 -0500

    controlador de compras

--- app/Http/Controllers/MarketController.php
+++ app/Http/Controllers/MarketController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\marketshopping;
 use App\Models\products;
+use App\Models\supplierinvoice;
 use App\Models\units;
 use FontLib\Table\Type\name;
 use Illuminate\Http\Request;
@@ -106,22 +107,68 @@ class MarketController extends Controller
         //dump($request);
         $day = $request->input('day');
         $supplier = $request->input('proveedor');
-        //dump($day, $supplier);
+        $invoicedate = $request->input('fechafactura');
+
+        $fecha = $request->input('fecha');
         if ($supplier != null) {
             $comprasdeldia = marketshopping::where('id', $supplier)->get();
+            $facturasdeldia = supplierinvoice::where('invoice_day', $fecha)->get();
         } else {
             $comprasdeldia = marketshopping::where('shoppingday', $day)->get();
+            $facturasdeldia = supplierinvoice::where('invoice_day', $day)->get();
         }
-        //dump($comprasdeldia);
-        $opciones = units::all();
-        $opciones2 = products::all();
-        return view('modules/market/marketinvoice', compact('comprasdeldia', 'opciones', 'opciones2'));
+        if ($invoicedate != null) {
+            /* dump($invoicedate);
+            dump($request->input('nombreProveedor'));
+            dump($request->input('numerofactura'));
+            dump(json_encode($request->input('producto')));
+            dump(json_encode($request->input('unidad')));
+            dump(json_encode($request->input('cantidad')));
+            dump(json_encode($request->input('canfac')));
+            dump(json_encode($request->input('dif')));
+            dump(json_encode($request->input('precio')));
+            dump(json_encode($request->input('metododepago')));
+            dump(json_encode($request->input('archivosimg')));
+            dump($request->totaldiferencia);
+            dump($request->total);
+            dump($request->input('total_credito'));
+            dump($request->input('total_efectivo')); */
+
+            $supplierinvoice = new supplierinvoice;
+            $supplierinvoice->invoice_day = $invoicedate;
+            $supplierinvoice->name_supplier = $request->input('nombreProveedor');
+            $supplierinvoice->invoice_number = $request->input('numerofactura');
+            $supplierinvoice->product = json_encode($request->input('producto'));
+            $supplierinvoice->unit = json_encode($request->input('unidad'));
+            $supplierinvoice->quantity = json_encode($request->input('cantidad'));
+            $supplierinvoice->invoice_quantity = json_encode($request->input('canfac'));
+            $supplierinvoice->difference = json_encode($request->input('dif'));
+            $supplierinvoice->price = json_encode($request->input('precio'));
+            $supplierinvoice->payment_type = json_encode($request->input('metododepago'));
+            $supplierinvoice->total_price = $request->total;
+            $supplierinvoice->total_quantity = $request->input('totalcan');
+            $supplierinvoice->total_invoice_quantity = $request->input('totalcanfac');
+            $supplierinvoice->total_credit = $request->input('total_credito');
+            $supplierinvoice->total_cash = $request->input('total_efectivo');
+            $supplierinvoice->file_image = json_encode($request->input('archivosimg'));
+            $supplierinvoice->save();
+            $request->session()->flash('mensaje', 'Datos de la factura guardada.');
+
+            $comprasdeldia = marketshopping::where('shoppingday', $invoicedate)->orderBy('created_at', 'desc')->get();
+            $facturasdeldia = supplierinvoice::where('invoice_day', $invoicedate)->orderBy('created_at', 'desc')->get();
+
+            //dd($comprasdeldia, $facturasdeldia);
+            return view('modules/market/marketinvoice', compact('comprasdeldia', 'facturasdeldia'));
+        }
+        return view('modules/market/marketinvoice', compact('comprasdeldia', 'facturasdeldia'));
     }
 
     public function showcreditinvoice(Request $request)
     {
-        dump($request);
-        return view('modules/market/creditinvoice');
+        //dump($request);
+        dump($request->input('name_supplier'));
+        $facturasdeldia = supplierinvoice::where('name_supplier', $request->input('name_supplier'))->orderBy('created_at', 'desc')->get();
+        return view('modules/market/creditinvoice', compact('facturasdeldia'));
     }
 
     public function showbudget()
