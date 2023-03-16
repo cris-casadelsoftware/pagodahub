91f25f69527a4cebaa677beab1b834156e7946e8
cris-casadelsoftware <cristopher@casadelsoftware.com>
2023-03-16 17:22:46 -0500

    migracion de las nuevas tablas para banco

---
+++ database/migrations/2023_03_14_162020_create_bankrequest_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bankrequests', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->longText('fromdate')->nullable();
+            $table->longText('todate')->nullable();
+            $table->longText('weeknumber')->nullable();
+            $table->double('cash1', 8, 2)->nullable();
+            $table->double('cash5', 8, 2)->nullable();
+            $table->double('roll0_01', 8, 2)->nullable();
+            $table->double('roll0_05', 8, 2)->nullable();
+            $table->double('roll0_10', 8, 2)->nullable();
+            $table->double('roll0_25', 8, 2)->nullable();
+            $table->double('roll0_50', 8, 2)->nullable();
+            $table->double('roll1_00', 8, 2)->nullable();
+            $table->longText('file_img')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bankrequest');
+    }
+};
---
+++ database/migrations/2023_03_16_165034_create_bankdeposit_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bankdeposits', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->longText('dates')->nullable();
+            $table->longText('banks')->nullable();
+            $table->double('cash', 8, 2)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bankdeposit');
+    }
+};
